822e4bbb790f44913b422a156237039d625c3aef
Mario Rivera Garcia <cruz.riga@hotmail.com>
2019-07-21 10:35:58 -0500

    Update ListaCitas.js

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Cita from './Cita';
 
 const ListaCitas = ({citas, eliminarCita}) => {
+    const titulo = Object.keys(citas).length === 0 ? 'No hay Citas Registradas':' Administrar Listado de Citas'
     return ( 
        <div className="card mt-2 py-5">
            <div className="card-body">
                <h2 className="card-title text-center">
-                   Administrar Listado de Citas
+                  {titulo}
                </h2>
                <div className="lista-citas">
                     {citas.map(cita =>{
